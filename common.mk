fbf76c4e14a544a5840f3f7b962c1a054de38001
Chenyang Zhong <zhongcy95@gmail.com>
2021-01-24 09:57:51 +0300

    sdm632-common: Drop soundtrigger HAL
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>

--- common.mk
+++ common.mk
@@ -78,8 +78,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@6.0-impl:32 \
     android.hardware.audio.effect@2.0-service \
-    android.hardware.soundtrigger@2.2-impl \
-    android.hardware.soundtrigger@2.2-service \
     audio.a2dp.default \
     audio.primary.msm8953 \
     audio.r_submix.default \
