b9a599e96c285d27a21bb577445e382c96fc354a
Jarl-Penguin <jarlpenguin@outlook.com>
2021-03-20 11:22:29 +0300

    sdm632-common: Bring back LiveDisplay
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>
    Change-Id: I93a516b6869c15e9155066588af36c8edb43aa3c

--- common.mk
+++ common.mk
@@ -254,6 +254,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.msm8953
 
+# LiveDisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.0-service-sysfs
+
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/media/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml \
